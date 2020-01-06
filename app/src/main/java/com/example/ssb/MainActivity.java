c46f38daa7f86d3eb7a18b2a07afbe3f0f7be6e5
mishra5047 <mishra695047@gmail.com>
2020-01-06 21:46:43 +0530

    Final Product

--- app/src/main/java/com/example/ssb/MainActivity.java
+++ app/src/main/java/com/example/ssb/MainActivity.java
@@ -1,35 +1,31 @@
 package com.example.ssb;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
-import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 import com.mikhaellopez.circularimageview.CircularImageView;
 
 public class MainActivity extends Activity {
-CircularImageView circularImageView;
-
+ImageView circularImageView;
+Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       circularImageView = (CircularImageView) findViewById(R.id.logo_edited);
-   new Handler().postDelayed(new Runnable() {
+       circularImageView = findViewById(R.id.logo_edited);
+
+       new Handler().postDelayed(new Runnable() {
        @Override
        public void run() {
-  Intent intent = new Intent(MainActivity.this,activity_2.class);
+  Intent intent = new Intent(MainActivity.this, Activity_2.class);
   startActivity(intent);
+  Animatoo.animateSlideUp(MainActivity.this);
 
        }
    },2500);
